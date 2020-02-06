adddfeec71acc1c90bf97df2894385bb176df412
Shahan Neda <shahan.neda@gmail.com>
2020-02-05 20:29:43 -0800

    added more training and data functions

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
-from  keras.models import Sequential;
+from keras.models import Sequential;
+from keras.layers import Dense;
 
 class Main:
     def __init__(self):
@@ -13,11 +14,25 @@ class Main:
 
         self.model.compile(loss='categorical_crossentropy',
               optimizer='sgd',
-              metrics=['accuracy'])k
+              metrics=['accuracy'])
 
-        self.model.compile(loss=keras.losses.categorical_crossentropy,
-              optimizer=keras.optimizers.SGD(lr=0.01, momentum=0.9, nesterov=True))
+        self.model.compile(loss='categorical_crossentropy',
+              optimizer='sgd',
+              metrics=['accuracy'])
+        #self.model.compile(loss=keras.losses.categorical_crossentropy,
+        #      optimizer=keras.optimizers.SGD(lr=0.01, momentum=0.9, nesterov=True)) # use this if we want more control over the varaibles otherwise the other one is simpler
 
+    
+        ask = int(input("Choose: \n1)Load Data \n2)Train \n3)Predict"));
+        
+        if(ask == 3):
+             while True:
+                ask = input("Value: ");
+                if(ask == 'q'):
+                    break;
+                print(self.predict(ask));
+                
+        
 
     def createData(self):
         self.a = np.array([2,3,4,5]);
@@ -25,9 +40,8 @@ class Main:
     def train(self):
         self.model.fit((2,3,4,5,6), (3,4,5,6,7), epochs=5, batch_size=32)
 
-    def predict(self):
-        self.model.
-        classes = model.predict(x_test, batch_size=128)
-
+    def predict(self, value):
+        predictions = self.model.predict(value, batch_size=128) # batch size is how many it does at once
+        return predictions;
 
 main = Main();
